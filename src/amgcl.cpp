a7e862ea261ef37067fe073a4c180b129498dbea
hbp-sys <869020624@qq.com>
2023-03-12 03:56:25 +0800

    this is #1 commit

---
+++ src/amgcl.cpp
@@ -0,0 +1,5 @@
+//
+// Created by hbp on 3/9/23.
+//
+
+#include "amgcl.hpp"
---
+++ src/amgcl.hpp
@@ -0,0 +1,14 @@
+//
+// Created by hbp on 3/9/23.
+//
+
+#ifndef IPC_AMGCL_HPP
+#define IPC_AMGCL_HPP
+
+
+class amgcl {
+
+};
+
+
+#endif //IPC_AMGCL_HPP
